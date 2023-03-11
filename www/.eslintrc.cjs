d5bea4bd2dc95d6068fa4d55e52595fca4c9bf94
Archie <archie@veed.io>
2023-03-11 17:31:24 +0000

    eslint & add nav layout

--- www/.eslintrc.cjs
+++ www/.eslintrc.cjs
@@ -1,27 +1,51 @@
-module.exports = {
-  // ...
-  extends: [
-    // ...
-    "plugin:astro/recommended",
-  ],
-  // ...
+/** @type {import("@types/eslint").Linter.Config} */
+
+modules.exports = {
+  plugins: ["astro"],
   overrides: [
     {
       // Define the configuration for `.astro` file.
       files: ["*.astro"],
+      // Enable this plugin
+      plugins: ["astro"],
+      env: {
+        // Enables global variables available in Astro components.
+        node: true,
+        "astro/astro": true,
+        es2020: true,
+      },
       // Allows Astro components to be parsed.
-      parser: "astro-eslint-parser",
+      parser: require.resolve("astro-eslint-parser"),
       // Parse the script in `.astro` as TypeScript by adding the following configuration.
-      // It's the setting you need when using TypeScript.
       parserOptions: {
-        parser: "@typescript-eslint/parser",
+        parser: hasTypescriptEslintParser
+          ? "@typescript-eslint/parser"
+          : undefined,
         extraFileExtensions: [".astro"],
+        // The script of Astro components uses ESM.
+        sourceType: "module",
       },
       rules: {
-        // override/add rules settings here, such as:
-        // "astro/no-set-html-directive": "error"
+        // eslint-plugin-astro rules
+        // Enable base rules
       },
     },
-    // ...
+    {
+      // Define the configuration for `<script>` tag.
+      // Script in `<script>` is assigned a virtual file name with the `.js` extension.
+      files: ["**/*.astro/*.js", "*.astro/*.js"],
+      env: {
+        browser: true,
+        es2020: true,
+      },
+      parserOptions: {
+        sourceType: "module",
+      },
+      rules: {
+        // If you are using "prettier/prettier" rule,
+        // you don't need to format inside <script> as it will be formatted as a `.astro` file.
+        "prettier/prettier": "off",
+      },
+    },
   ],
 };
